ac265a19562efdc98c8202e704c70d6ef77a1598
Bernd Verst <bernd.verst@gsa.gov>
2016-07-25 17:28:21 -0700

    Minor fix to persist script.

--- persist_config.sh
+++ persist_config.sh
@@ -1,7 +1,7 @@
 APP_NAME="crm"
 
-cf files $APP_NAME app/htdocs/config_override.php | tail -n +3 | sed '/./,$!d' > ./.config_override.php
-cf files $APP_NAME app/htdocs/config.php | tail -n +3 | sed '/./,$!d' > ./.config.php
+cf files $APP_NAME app/htdocs/config_override.php | tail -n +3 | sed '/./,$!d' > ./config_override.php
+cf files $APP_NAME app/htdocs/config.php | tail -n +3 | sed '/./,$!d' > ./config.php
 cf files $APP_NAME app/htdocs/.htaccess | tail -n +3 | sed '/./,$!d' > ./.htaccess
 
 cf push $APP_NAME
